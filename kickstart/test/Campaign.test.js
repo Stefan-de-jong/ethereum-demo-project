2ece0b9f2034556407b234d5988a171cc8b142ae
Stefan <625583@student.inholland.nl>
2021-09-16 14:19:31 +0200

    More tests and fixed deploy script

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -38,4 +38,76 @@ describe("Campaigns", () => {
 		assert.ok(factory.options.address);
 		assert.ok(campaign.options.address);
 	});
+
+	it("marks creater as the campaign manager", async () => {
+		const manager = await campaign.methods.manager().call();
+		assert.equal(accounts[0], manager);
+	});
+
+	it("allow people to contribute money and marks them as approver", async () => {
+		await campaign.methods.contribute().send({
+			from: accounts[1],
+			value: "200",
+		});
+		const isContributor = await campaign.methods
+			.approvers(accounts[1])
+			.call();
+
+		assert.equal(true, isContributor);
+	});
+
+	it("requires a minimum contribution", async () => {
+		try {
+			await campaign.methods.contribute().send({
+				from: accounts[1],
+				value: "5",
+			});
+			assert(false);
+		} catch (err) {
+			assert(err);
+		}
+	});
+
+	it("allows a manager to make a payment request", async () => {
+		await campaign.methods
+			.createRequest(
+				"Buy batteries", //request description
+				"100", // request ammount
+				accounts[2] // request recipient of the payment
+			)
+			.send({
+				from: accounts[0],
+				gas: "1000000",
+			});
+		const request = await campaign.methods.requests(0).call();
+
+		assert.equal("Buy batteries", request.description);
+	});
+
+	it("processes requests", async () => {
+		await campaign.methods.contribute().send({
+			from: accounts[0],
+			value: web3.utils.toWei("10", "ether"),
+		});
+
+		await campaign.methods
+			.createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+			.send({ from: accounts[0], gas: "1000000" });
+
+		await campaign.methods.approveRequest(0).send({
+			from: accounts[0],
+			gas: "1000000",
+		});
+
+		await campaign.methods.finalizeRequest(0).send({
+			from: accounts[0],
+			gas: "1000000",
+		});
+
+		let balance = await web3.eth.getBalance(accounts[1]);
+		balance = web3.utils.fromWei(balance, "ether");
+		balance = parseFloat(balance);
+
+		assert(balance > 104);
+	});
 });
