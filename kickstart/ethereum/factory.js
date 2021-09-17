435db555107396be6d23a9161af713cb3ab6dcdf
Stefan <625583@student.inholland.nl>
2021-09-17 15:53:43 +0200

    Show page is working

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
 	JSON.parse(CampaignFactory.interface),
-	"0xeE80BEF78bcDaC32d2fDe1a9Ad80ddBf70f444E2"
+	"0xDA28cC6bd3eF6deE1FD4FFCad6eF0Fb3aA3386be"
 );
 
 export default instance;
